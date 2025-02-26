ce3386523477583ccfe785d87fabdd327406e5fd
Liza Hannoshyna <fox8pro8@gmail.com>
2025-02-26 22:02:03 +0200

    add-jsFile-fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import * as bootstrap from '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './js/header.js';
+import './js/games/leap-year-check.js';
+import './js/games/guess-the-number.js';
+import './js/games/rock-paper-scissors.js';
+import './js/games/simple-calculator.js';
+import './js/games/time-converter.js';
+import './js/games/dino-game.js';
+import './js/games/ball-move.js';
+import './js/games/max-of-three.js';
+import './js/games/image-slider.js';
+import './js/games/scientists-data.js';
+import './js/footer.js';
